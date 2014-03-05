a8810eb111a4ad3e8d6c32120f390a74ae7ed75c
rice2007 <rice2007@gmail.com>
2014-03-05 10:17:16 -0600

    Update RBTreeDriver.java

--- src/labseven/redblacktree/RBTreeDriver.java
+++ src/labseven/redblacktree/RBTreeDriver.java
@@ -8,23 +8,6 @@ import java.util.Scanner;
 public class RBTreeDriver {
 
     public static void main(String[] args) {
- /*       RBTree<Integer> tree = new RBTree<>();
-        System.out.println(tree.toString());
-
-        RBNode<Integer> node = new RBNode<>(16);
-        tree.insert(node);
-        tree.insert(new RBNode<Integer>(10));
-        tree.insert(new RBNode<Integer>(20));
-        tree.insert(new RBNode<Integer>(7));
-        tree.insert(new RBNode<Integer>(13));
-        tree.insert(new RBNode<Integer>(18));
-        tree.insert(new RBNode<Integer>(21));
-        tree.insert(new RBNode<Integer>(14));
-        tree.insert(new RBNode<Integer>(5));
-        tree.insert(new RBNode<Integer>(3));
-        tree.insert(new RBNode<Integer>(9));
-        System.out.println(tree.toString());*/
-
         RBTree<Long> csvTree = new RBTree<>();
         Scanner scan = null;
         String line;
