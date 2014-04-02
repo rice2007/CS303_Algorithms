e56f924040275ec467c94977d9b2731082831634
rice2007 <rice2007@gmail.com>
2014-04-02 00:43:47 -0500

    ten complete

--- src/labten/dfs/DFSDriver.java
+++ src/labten/dfs/DFSDriver.java
@@ -1,10 +1,8 @@
 package labten.dfs;
 
-import labnine.graph.DirectedGraph;
 import labnine.graph.UndirectedGraph;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -12,14 +10,17 @@ public class DFSDriver {
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(
-                new FileReader("tinyDG.txt"));
+                new FileReader("C:\\Users\\Ajay\\Documents\\GitHub\\CS303_Algorithms\\src\\labnine\\graph\\tinyDG.txt"));
         UndirectedGraph uGraph = new UndirectedGraph(br);
         System.out.println(uGraph.toString());
         DepthFirstPaths dfp = new DepthFirstPaths(uGraph, 0);
         for (int i = 0; i < uGraph.getV(); i++) {
             if (dfp.hasPathTo(i)) {
                 System.out.println(dfp.getSource() + " to " + i + ": " + dfp.pathTo(i));
+            } else {
+                System.out.println("No path from " + dfp.getSource() + " to " + i);
             }
         }
+        System.out.println(dfp.topologicalSort());
     }
 }
--- src/labten/dfs/DepthFirstPaths.java
+++ src/labten/dfs/DepthFirstPaths.java
@@ -2,14 +2,19 @@ package labten.dfs;
 
 import labnine.graph.Graph;
 
-import java.util.Stack;
+import java.util.LinkedList;
 
 public class DepthFirstPaths {
-    private boolean[] marked;    // marked[v] = is there an s-v path?
-    private int[] distTo;
-    private int[] edgeTo;        // edgeTo[v] = last edge on s-v path
+
+    private final boolean[] grayed;    // marked[v] = is there an s-v path?
+    private final boolean[] marked;
+    private final int NIL = Integer.MIN_VALUE;
     private final int source;         // source vertex
+    private final int[] distTo;
+    private final int[] edgeTo;        // edgeTo[v] = last edge on s-v path
+
     private int time;
+    private LinkedList<Integer> topList;
 
     /**
      * Computes a path between <tt>s</tt> and every other vertex in graph <tt>G</tt>.
@@ -18,17 +23,18 @@ public class DepthFirstPaths {
      */
     public DepthFirstPaths(Graph G, int s) {
         marked = new boolean[G.getV()];
+        grayed = new boolean[G.getV()];
         distTo = new int[G.getV()];
         edgeTo = new int[G.getV()];
+        topList = new LinkedList<>();
         source = s;
-        dfs(G, s);
+        dfs(G);
     }
 
     // depth first search from v
-    private void dfs(Graph G, int v) {
-        for (int u = 0; u < G.getV(); u++) {
-            marked[u] = false;
-            edgeTo[u] = 0;
+    private void dfs(Graph G) {
+        for (int i = 0; i < edgeTo.length; i++) {
+            edgeTo[i] = NIL;
         }
         time = 0;
         for (int u = 0; u < G.getV(); u++) {
@@ -40,16 +46,21 @@ public class DepthFirstPaths {
 
     private void DFSVisit(Graph G, int u) {
         time++;
-        distTo[u] = time;
+        grayed[u] = true;
         for (int v : G.adj(u)) {
-            if (!marked[v]) {
+            if (!marked[v] && !grayed[v]) {
                 edgeTo[v] = u;
-                marked[v] = true;
                 DFSVisit(G, v);
             }
         }
         marked[u] = true;
         time++;
+        distTo[u] = time;
+        topSortHelper(u);
+    }
+
+    private void topSortHelper(int u) {
+        topList.push(u);
     }
 
     /**
@@ -58,7 +69,14 @@ public class DepthFirstPaths {
      * @return <tt>true</tt> if there is a path, <tt>false</tt> otherwise
      */
     public boolean hasPathTo(int v) {
-        return marked[v];
+        if (v == NIL) {
+            return false;
+        }
+        if (v != source) {
+            return hasPathTo(edgeTo[v]);
+        } else {
+            return true;
+        }
     }
 
     /**
@@ -69,10 +87,10 @@ public class DepthFirstPaths {
      *   <tt>s</tt> and vertex <tt>v</tt>, as an Iterable
      */
     public Iterable<Integer> pathTo(int v) {
-        if (!this.hasPathTo(v)) {
+        if (!hasPathTo(v)) {
             return null;
         }
-        Stack path = new Stack();
+        LinkedList<Integer> path = new LinkedList<>();
         for (int i = v; i != source ; i = edgeTo[i]) {
             path.push(i);
         }
@@ -84,9 +102,7 @@ public class DepthFirstPaths {
         return source;
     }
 
-    /**
-     * Unit tests the <tt>labten.dfs.DepthFirstPaths</tt> data type.
-     */
-    
-
+    public LinkedList<Integer> topologicalSort() {
+        return topList;
+    }
 }
